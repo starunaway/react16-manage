df1bf9473339f612f54f3ff7350f4b491c7640b7
keeping <starunaway@gmail.com>
2020-05-12 23:15:51 +0800

    feat: 🎸 route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
-import {Button, message} from 'antd';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Login from './pages/login/login';
+import Admin from './pages/admin/admin';
+
 export default class App extends Component {
-  handleClick = () => {
-    message.success('click');
-  };
   render() {
     return (
-      <Button type='primary' onClick={this.handleClick}>
-        hellpo
-      </Button>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/pages/login/login.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+/**
+ * 登录路由
+ *
+ * */
+export default class Login extends Component {
+  render() {
+    return <div>Login</div>;
+  }
+}
