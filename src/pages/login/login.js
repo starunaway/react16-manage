1d1ca7fa82374bc3c2d41eca9a9037e8d1477b05
keeping <starunaway@gmail.com>
2020-05-12 23:32:32 +0800

    feat: 🎸 update

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-
+import './login.less';
 /**
  * 登录路由
- *
- * */
+ **/
 export default class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className='login'>
+        <header className='login-header'>后台管理系统</header>
+        <section className='login-content'> </section>
+      </div>
+    );
   }
 }
