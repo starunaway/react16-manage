110ed60e25ba8dc7e608edaac3bfac856b20893d
keeping <starunaway@gmail.com>
2020-05-21 23:56:32 +0800

    feat: 🎸 admin layout

---
+++ src/components/left-nav/index.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import './index.less';
+class LeftNav extends Component {
+  render() {
+    return <div className='left-nav'>LeftNav</div>;
+  }
+}
+
+export default LeftNav;
--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import {Redirect} from 'react-router-dom';
 import memoryUtils from '../../utils/memoryUtils';
+import LeftNav from '../../components/left-nav';
+import Header from '../../components/header';
+import {Layout} from 'antd';
+const {Footer, Sider, Content} = Layout;
 
 export default class Admin extends Component {
   render() {
@@ -9,6 +13,17 @@ export default class Admin extends Component {
       // 当前没有登录
       return <Redirect to='/login'></Redirect>;
     }
-    return <div>Hello, {user.username}</div>;
+    return (
+      <Layout style={{height: '100%'}}>
+        <Sider>
+          <LeftNav></LeftNav>
+        </Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content style={{backgroundColor: '#fff'}}>Content</Content>
+          <Footer style={{textAlign: 'center', color: '#ccc'}}>推荐使用Chrome浏览器,可以获得更好的体验</Footer>
+        </Layout>
+      </Layout>
+    );
   }
 }
