3733b76499fdee3c8035e31dba6605e0731cf416
keeping <starunaway@gmail.com>
2020-06-01 18:35:03 +0800

    feat: 🎸 router

--- src/pages/product/detail.jsx
+++ src/pages/product/detail.jsx
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
+import {Button, Input} from 'antd';
 
 class ProductDetail extends Component {
   render() {
-    return <div className='header'>ProductDetail</div>;
+    return (
+      <div className='product-detail'>
+        <Button type='primary'></Button>
+        <Input></Input>
+      </div>
+    );
   }
 }
 
--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -6,6 +6,8 @@ import ProductAddUpdate from './add-update';
 
 class Product extends Component {
   render() {
+    //   Router 路由判断路径的时候，会判断当前path是什么，根据path判断渲染哪个组件
+    //  组件也可以继续使用router判断下一级路由应该渲染什么组件，但是不能写父组件对应的路由，死循环
     return (
       <Switch>
         <Route exact path='/product' component={ProductHome}></Route>
