40050da0ebf78d33778d52dd17a40f3a675f245f
keeping <starunaway@gmail.com>
2020-07-08 23:53:01 +0800

    feat: 🎸 react-redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './App';
 
 import storageUtils from './utils/storageUtils';
@@ -8,4 +9,10 @@ import memoryUtils from './utils/memoryUtils';
 const user = storageUtils.getUser();
 memoryUtils.user = user;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App></App>
+  </Provider>,
+
+  document.getElementById('root')
+);
